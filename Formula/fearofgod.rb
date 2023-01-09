1feac257fe0bf31d1f2237aa0967b609288065eb
CirroBuild <122240402+CirroBuild@users.noreply.github.com>
2023-01-08 20:31:53 -0800

    v1.1

--- Formula/fearofgod.rb
+++ Formula/fearofgod.rb
@@ -1,8 +1,8 @@
 class Fearofgod < Formula
   desc "FearofGod Test"
   homepage "https://fearofgod.com"
-  url "https://github.com/CirroBuild/FoG/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "41f05f352a14f346971d1840df1b44b87aec736f9e9e3c4b679a88648bafb3ed"
+  url "https://github.com/CirroBuild/FoG/releases/download/v1.1.0/FGM.tar.gz"
+  sha256 "84fd4fb16d2d59e5436d5942bfd578769944606546dbef0b04ade19dd84c8de4"
   license "MPL-2.0"
 
   # depends_on "cmake" => :build
