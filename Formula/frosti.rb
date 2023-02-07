b62830a3c8dac197d2bf7f5afa481957a77fe128
CirroBuild <122240402+CirroBuild@users.noreply.github.com>
2023-02-06 23:55:53 -0800

    Update frosti.rb

--- Formula/frosti.rb
+++ Formula/frosti.rb
@@ -1,17 +1,17 @@
 class Frosti < Formula
   desc "Frosti Test"
   homepage "https://frostibuild.com"
-  version "1.6.preview"
+  version "1.7.preview"
   license "MPL-2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/CirroBuild/FoG/releases/download/v1.6.preview/Frosti-v1.6.preview-x64.tar.gz"
-      sha256 "0043d8adb816e569574585303c856ad590569da0c3f96b097f3d697077ce6d66"
+      url "https://github.com/CirroBuild/FoG/releases/download/v1.7.preview/Frosti-v1.7.preview-x64.tar.gz"
+      sha256 "e61e616d3020eb03256b2a43d5db5f9b4a917c4a0812ba0e6e133a0b4a7b8490"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/CirroBuild/FoG/releases/download/v1.6.preview/Frosti-v1.6.preview-arm64.tar.gz"
-      sha256 "ffdb089c5f33a40e7160f26b6a0636be227dc87d294e2a7f938c648d9225a897"
+      url "https://github.com/CirroBuild/FoG/releases/download/v1.7.preview/Frosti-v1.7.preview-arm64.tar.gz"
+      sha256 "540bef08596fba94ddb62096bff6a9695e8bb5dc3e4eb1ca0cd7b5dbad3e4000"
     end
   end
 
